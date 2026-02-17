145aad5ba4b177114da0faa8de5bb48531cc20e2
William Chenault <146786825+CthulhuCode@users.noreply.github.com>
2026-02-17 20:34:35 +0000

    initial commit and base shell code

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+        int gridNums[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};       //Array to show the users which number to enter to choose a spot
+        char board[3][3];       //Array to save X & O placements as the game progresses
+        int player1 = 0;        //Track player 1's wins
+        int player2 = 0;        //Track player 2's wins
+        char input;     //User input
+
+        //Fill board array with dashes
+
+
+        printf("-----------------------\n");
+        printf("----  Tic-Tac-Toe  ----\n");
+        printf("-----------------------\n");
+        //Print the grid numbers before the game starts (call function)
+        ////Only print once at the beginning of each game
+
+
+        //Game Logic
+
+        //Prompt players for spots and save
+
+
+
+
+        //Ask to play again
+
+
+
+
+
+        return 0;
+}
